9838fabed6baef0f24bfb2baf49ce102d54a6118
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2024-03-11 20:41:40 +1300

    Add simple service example.

---
+++ examples/hello/hello.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env async-service
+
+class SleepService < Async::Service::Generic
+	def setup(container)
+		super
+		
+		container.run(count: 1, restart: true) do |instance|
+			instance.ready!
+
+			while true
+				puts "Hello World!"
+				sleep 1
+			end
+		end
+	end	
+end
+
+service "sleep" do
+	service_class SleepService
+end
